e1efd2cea07c50512602545154ac8f81a8d921f5
unknown <shcherbakovkirill2@gmail.com>
2024-03-22 16:17:25 +0300

    fix text avatar styles

--- lib/components/Avatars/TextAvatar/ui/TextAvatar.ui.tsx
+++ lib/components/Avatars/TextAvatar/ui/TextAvatar.ui.tsx
@@ -1,45 +1,23 @@
-import { FC } from 'react'
+import { FC } from "react";
 
-import { useOneLetter } from '../logic/useOneLetter.logic'
+import { useOneLetter } from "../logic/useOneLetter.logic";
 
-import type { TextAvatarType } from './TextAvatar.types'
+import type { TextAvatarType } from "./TextAvatar.types";
 
+import s from "../assets/style.module.css";
 
-export const TextAvatar: FC<TextAvatarType> = ({ bgColor = 'green', name, auto = false, sizeType = 'md', ...props}) => {
+export const TextAvatar: FC<TextAvatarType> = ({
+  bgColor = "green",
+  name,
+  auto = false,
+  sizeType = "md",
+  ...props
+}) => {
+  const letter = useOneLetter(name);
 
-    const letter = useOneLetter(name)
-
-    const classsBg = {
-        green: 'bg-avatar-green',
-        pink: 'bg-avatar-pink',
-        yellow: 'bg-avatar-yellow',
-        blue: 'bg-avatar-blue',
-        purple: 'bg-avatar-purple',
-        lightPurple: 'bg-avatar-lightPurple'
-    }
-
-    const classSizes = {
-       es: 'w-7 h-7 text-xs',
-       sm: 'w-10 h-10 text-lg',
-       md: 'w-14 h-14 text-2xl',
-       lg: 'w-18 h-18 text-3xl'
-    }
-
-    return (
-
-        <article 
-         {...props} 
-         className={`
-           rounded-circle select-none ${classsBg[bgColor]} ${classSizes[sizeType]}
-           flex justify-center items-center font-medium pb-0.25 cursor-pointer
-         `}
-         >
-            {
-               auto ?
-                letter
-                :
-                name
-            }
-        </article>
-    )
-}
+  return (
+    <article {...props} className={`${s.root} ${s[bgColor]} ${s[sizeType]}`}>
+      {auto ? letter : name}
+    </article>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { TextAvatar } from "../lib/main";
+
 function App() {
-  return <div style={{ padding: 320, background: "#1F1F24" }}>Hello</div>;
+  return (
+    <div style={{ padding: 320, background: "#1F1F24" }}>
+      <TextAvatar auto name="Rfrfer" bgColor="green" sizeType="lg" />
+    </div>
+  );
 }
 
 export default App;
