18b452d2d3aa2a3bbeab5979df05d3d61ce4b988
unknown <shcherbakovkirill2@gmail.com>
2024-04-08 13:12:57 +0300

    fix: select compoonent

--- lib/components/Input/ui/main.component.tsx
+++ lib/components/Input/ui/main.component.tsx
@@ -1,37 +1,36 @@
-import { FC, InputHTMLAttributes, ReactNode } from 'react'
+import { FC, InputHTMLAttributes, ReactNode } from "react";
 
-import s from '../assets/input.module.css'
-
+import s from "../assets/input.module.css";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-    isError: boolean 
-    variant: 'lg' | 'sm'
-    icon?: ReactNode
-    cbIcon?: () => void 
-    disabled?: boolean
+  isError: boolean;
+  variant: "lg" | "sm" | "full";
+  icon?: ReactNode;
+  cbIcon?: () => void;
+  disabled?: boolean;
 }
 
-
-export const MainInputComponent: FC<Props> = ({ 
-    variant = 'lg', isError = false, icon, cbIcon, disabled = false, ...props 
+export const MainInputComponent: FC<Props> = ({
+  variant = "lg",
+  isError = false,
+  icon,
+  cbIcon,
+  disabled = false,
+  ...props
 }) => {
-
-    return (
-
-        <div className={`${s.wrapper} ${disabled && s.dis}`}>
-            <input 
-             className={`${s.root} ${s[variant]} ${isError && s.error} ${icon && s.pad}`} 
-             {...props} 
-             />
-             {
-                icon &&
-                  <div 
-                   onClick={() => cbIcon?.()} 
-                   className={`${s.icon} ${s[variant]}`}
-                   >
-                    {icon}
-                  </div>
-             }
+  return (
+    <div className={`${s.wrapper} ${disabled && s.dis}`}>
+      <input
+        className={`${s.root} ${s[variant]} ${isError && s.error} ${
+          icon && s.pad
+        }`}
+        {...props}
+      />
+      {icon && (
+        <div onClick={() => cbIcon?.()} className={`${s.icon} ${s[variant]}`}>
+          {icon}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { TextField } from "../lib/main";
+
 function App() {
-  return <div style={{ padding: 20, background: "#343741" }}></div>;
+  return (
+    <div style={{ padding: 20, background: "#343741" }}>
+      <TextField variant="select" placeholder="Select" sizeType="full" />
+    </div>
+  );
 }
 
 export default App;
