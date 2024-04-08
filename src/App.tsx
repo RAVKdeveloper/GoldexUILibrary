130d24d7b5fd616301b1e48e361cbba03d89d45b
unknown <shcherbakovkirill2@gmail.com>
2024-04-08 14:55:49 +0300

    fix: inputs bag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { TextField } from "../lib/main";
+import { EyeIcon } from "goldex-icon-library";
+
 function App() {
-  return <div style={{ padding: 20, background: "#343741" }}></div>;
+  return (
+    <div style={{ padding: 20, background: "#343741", width: 500 }}>
+      <TextField
+        icon={<EyeIcon width={20} height={20} opacity={0.4} />}
+        sizeType="full"
+        variant="select"
+      />
+    </div>
+  );
 }
 
 export default App;
