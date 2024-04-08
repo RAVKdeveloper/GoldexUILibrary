d133586be90e11a7adc94ea04a9ccda8b55ca0ea
unknown <shcherbakovkirill2@gmail.com>
2024-04-08 16:53:13 +0300

    release: version 1.1.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import { TextField } from "../lib/main";
-import { EyeIcon } from "goldex-icon-library";
-
 function App() {
   return (
-    <div style={{ padding: 20, background: "#343741", width: 500 }}>
-      <TextField
-        icon={<EyeIcon width={20} height={20} opacity={0.4} />}
-        sizeType="full"
-        variant="select"
-      />
-    </div>
+    <div style={{ padding: 20, background: "#343741", maxWidth: 1431 }}></div>
   );
 }
 
