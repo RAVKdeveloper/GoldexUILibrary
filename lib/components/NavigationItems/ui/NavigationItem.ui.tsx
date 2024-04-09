3ceca44e98f6b17165b06e13c03adac21cea6d5c
unknown <shcherbakovkirill2@gmail.com>
2024-04-09 14:00:57 +0300

    fix: navItems component

--- lib/components/NavigationItems/ui/NavigationItem.ui.tsx
+++ lib/components/NavigationItems/ui/NavigationItem.ui.tsx
@@ -1,55 +1,57 @@
-import { FC } from 'react'
+import { FC } from "react";
 
-import type { NavigationItemType } from '../assets/NavigationItem.type' 
+import type { NavigationItemType } from "../assets/NavigationItem.type";
 
-import { useNavItem } from '../logic/useNavItem.model'
+import { useNavItem } from "../logic/useNavItem.model";
 
-import s from '../assets/style.module.css'
-
+import s from "../assets/style.module.css";
 
 export const NavigationItem: FC<NavigationItemType> = ({
-    Icon, listNodes, badge, isEnd = false, text, isActive = false, disabled = false, type = 'main', keyOpen = 0, auto = false
+  Icon,
+  listNodes,
+  badge,
+  isEnd = false,
+  text,
+  isActive = false,
+  disabled = false,
+  type = "main",
+  keyOpen = 0,
+  auto = false,
 }) => {
+  const { open, isOpen } = useNavItem(keyOpen);
 
-    const { open, isOpen } = useNavItem(keyOpen)
+  const isOpenedList = isOpen.keyOpen === keyOpen && isOpen.isOpen;
 
-    const isOpenedList = isOpen.keyOpen === keyOpen && isOpen.isOpen
-
-    return (
-
-        <li onClick={open} className={s.wrapper}>
-        <div className={`
+  return (
+    <li className={s.wrapper}>
+      <div
+        className={`
          ${s.root} 
          ${s[type]}
          ${isActive && s.active}
          ${auto && isOpenedList && s.active}
          ${disabled && s.dis}
-         ${Icon ? '' : isEnd ? s.endBorder : s.middleBorder}
-         `}>
-            <div className={s.content}>
-            {
-                Icon && 
-                <Icon 
-                 width={20} 
-                 height={20} 
-                 opacity={isActive || (isOpenedList && auto) ? 1 : 0.4} 
-                 /> 
-            }    
-            {text}
-            </div>
-            <div className={s.badge}>
-            {
-                badge &&
-                 badge
-            }
-            </div>
+         ${Icon ? "" : isEnd ? s.endBorder : s.middleBorder}
+         `}
+        onClick={open}
+      >
+        <div className={s.content}>
+          {Icon && (
+            <Icon
+              width={20}
+              height={20}
+              opacity={isActive || (isOpenedList && auto) ? 1 : 0.4}
+            />
+          )}
+          {text}
         </div>
-        {
-           listNodes && 
-            <ul className={`${s.listNodes} ${isOpenedList && s.open}`}>
-                {listNodes}
-            </ul>
-        }
-        </li>
-    )
-}
+        <div className={s.badge}>{badge && badge}</div>
+      </div>
+      {listNodes && (
+        <ul className={`${s.listNodes} ${isOpenedList && s.open}`}>
+          {listNodes}
+        </ul>
+      )}
+    </li>
+  );
+};
