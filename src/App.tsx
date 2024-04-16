0e62a86fcb3b46581c33de2a5cc858c99f8073cb
unknown <shcherbakovkirill2@gmail.com>
2024-04-16 15:44:48 +0300

    release: version 1.1.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { NumericCard } from "../lib/main";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-[#1F1F24] p-10 flex items-center gap-5 flex-wrap lg:flex-nowrap">
+      <NumericCard
+        totalBalance={435}
+        term="eweter"
+        chart={<div style={{ height: 100 }}></div>}
+        profitability={545}
+        textTotal="wetetler"
+      />
+      <NumericCard
+        totalBalance={435}
+        term="eweter"
+        chart={<div style={{ height: 100 }}></div>}
+        profitability={545}
+        textTotal="wetetler"
+      />
+      <NumericCard
+        totalBalance={435}
+        term="eweter"
+        chart={<div style={{ height: 100 }}></div>}
+        profitability={545}
+        textTotal="wetetler"
+      />
+    </div>
+  );
 }
 
 export default App;
